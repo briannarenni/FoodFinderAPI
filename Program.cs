f3da6c37eed0557c5deceab8fc1ff9d295530cf1
Brianna Renni <briannaxrene@icloud.com>
2023-01-13 22:18:23 -0800

    Create DB, set up Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,43 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-app.MapGet("/", () => "Hello World!");
+WebApplicationBuilder? builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+// builder.Services.AddDbContext<PlayerDB>(options => options.UseInMemoryDatabase("players"));
+string? connString = builder.Configuration.GetValue<string>("ConnectionStrings:FoodFinderDB");
+
+// App instance
+WebApplication app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    builder.Configuration.AddUserSecrets<Program>();
+}
+
+// app.MapGet("/restaurants", async (FoodFinderDB db) => await db.Players.ToListAsync());
+// app.MapGet("/ratings", async (FoodFinderDB db) => await db.Players.ToListAsync());
+// app.MapGet("/menus", async (FoodFinderDB db) => await db.Players.ToListAsync());
 
 app.Run();
+
+
+// app.MapPost("/players", async (PlayerDB db, Player player) =>
+// {
+//     await db.Players.AddAsync(player);
+//     await db.SaveChangesAsync();
+//     return Results.Created($"/todo/player.playerId", player);
+// });
+
+// app.MapPatch("/players/{id}", async (PlayerDB db, int playerId, int addXP) =>
+// {
+//     var player = await db.Players.FindAsync(playerId);
+//     if (player is null) return Results.NotFound();
+//     player.totalXP += addXP;
+//     await db.SaveChangesAsync();
+
+//     return Results.NoContent();
+// });
